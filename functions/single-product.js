455b5a0416415e036f4ba2559fd0eace106c1ddb
MrEmin <emuhammet.ozdemir@gmail.com>
2023-08-27 14:27:54 +0300

    Single Product - Structure

--- functions/single-product.js
+++ functions/single-product.js
@@ -1,6 +1,15 @@
 exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters
+
+  if (id) {
     return {
-        statusCode: 200,
-        body: 'single product route',
+      statusCode: 200,
+      body: 'single product',
     }
+  }
+
+  return {
+    statusCode: 400,
+    body: 'Please provide product id',
+  }
 }
